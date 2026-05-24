98b068de9de8fb9c5380e47b1d0f9cb6bd4339b6
Sewmini Kumaranayaka <sewminikumaranayaka13@gmail.com>
2026-05-24 12:56:12 +0530

    chore: update eslint configuration to include ignores for dist and node_modules directories

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,9 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
   js.configs.recommended,
   {
     files: ['src/**/*.{js,jsx}'],
